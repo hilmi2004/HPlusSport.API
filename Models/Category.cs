e31c950e6e0e275a036e864ebf124e785d9f7dd2
hilmi gabi <gabihilmi4@gmail.com>
2024-11-01 09:35:44 +0100

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using HPlusSport.API.Models;
+
+namespace HPlusSport.API.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public virtual List<Product> Products { get; set; }
+    }
+}
